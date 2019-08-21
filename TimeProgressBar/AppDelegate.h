9d57926cfc52a926293b6fcf41aab467552f4a50
zhangrunqing <zhangrunqing2009@126.com>
2019-08-21 09:37:40 +0800

    Initial Commit

---
+++ TimeProgressBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeProgressBar
+//
+//  Created by zrq on 2019/8/21.
+//  Copyright © 2019 zrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeProgressBar/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TimeProgressBar
+//
+//  Created by zrq on 2019/8/21.
+//  Copyright © 2019 zrq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
